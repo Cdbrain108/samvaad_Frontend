a7ba59732d1d45116e88aca9361616f6003d3654
Anuj Kesharwani <anuj@example.com>
2026-09-09 09:22:42 +0530

    build: add unique suffix for cache busting

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash]-gita.js',
+        chunkFileNames: 'assets/[name]-[hash]-gita.js',
+        assetFileNames: 'assets/[name]-[hash]-gita.[ext]',
+      },
+    },
+  },
   server: {
     port: 5173,
     proxy: {
